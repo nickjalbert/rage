2e32652651cb65d3fe8a3d44b822ba154a7befec
Nick Jalbert <nickjalbert@gmail.com>
2010-04-13 17:36:38 -0700

    figured out how to access CALayer to get image information I need

--- iphone/Rage/Classes/RageView.h
+++ iphone/Rage/Classes/RageView.h
@@ -7,17 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MyCLController.h"
 
-
-@interface RageView : UIViewController {
+@interface RageView : UIViewController <MyCLControllerDelegate>{
+  MyCLController * locationController;
     NSMutableArray * img_array;
+  IBOutlet UILabel *  locationLabel;
     IBOutlet UIImageView * my_img;
     IBOutlet UIImageView * animation_img;
+  BOOL next_direction_is_up;
+  CALayer * mylayer;
 }
 
 @property (nonatomic, retain) IBOutlet UIImageView * my_img;
 @property (nonatomic, retain) IBOutlet UIImageView * animation_img;
+@property (nonatomic, retain) IBOutlet UILabel * locationLabel;
 -(IBAction)rageOn:(id)sender;
-
+-(void)locationUpdate:(CLLocation *) location;
+-(void)locationError:(NSError *) error;
 
 @end
--- iphone/Rage/Classes/RageView.m
+++ iphone/Rage/Classes/RageView.m
@@ -13,9 +13,23 @@
 @implementation RageView
 @synthesize my_img;
 @synthesize animation_img;
+@synthesize locationLabel;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  
+  next_direction_is_up = YES;
+  /*
+  mylayer = [CALayer layer];
+  UIImage * mylayer_img = [UIImage imageNamed:@"rage10.png"];
+  mylayer.contents = (id) mylayer_img.CGImage;
+  mylayer.bounds = CGRectMake(0, 0, mylayer_img.size.width, mylayer_img.size.height);
+  
+  [self.view.layer addSublayer:mylayer];
+  locationController = [[MyCLController alloc] init];
+  locationController.delegate =self;
+  [locationController.locationManager startUpdatingLocation];
+  */
 
     img_array = [[NSMutableArray alloc] initWithCapacity:NUMPICS];
     for (int i = NUMPICS; i >= 0; i--) {
@@ -24,17 +38,43 @@
 
     UIImageView * bkg = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 20, 100)];
     [self.view addSubview:bkg];
-    [bkg setBackgroundColor:[UIColor whiteColor]];
+  [bkg setBackgroundColor:[UIColor whiteColor]];
     [bkg release];
 
     animation_img = [[UIImageView alloc] initWithFrame:CGRectMake(10,10,20,100)];
     [self.view addSubview:animation_img];
-    [animation_img setBackgroundColor:[UIColor redColor]];
+  [animation_img setBackgroundColor:[UIColor colorWithRed:(221.0f/255.0f) green:(21.0f/255.0f) blue:(21.0f/255.0f) alpha:1.0]];
+     
 }
 
 - (IBAction)rageOn:(id)sender {
   
-  [animation_img.layer 
+  CALayer * lay = [animation_img layer];
+  CALayer * mypre = [lay presentationLayer];
+  //CGRect visRect =[mylayer visibleRect];  
+  
+  
+  CGRect mybd = [mypre bounds];
+  //CGRect mybd = [[[animation_img  layer]  presentationLayer] bounds];
+  NSLog(@"pres rect: %@", NSStringFromCGRect(mybd));
+  NSLog(@"layout rect: %@", NSStringFromCGRect([lay bounds]));
+  [animation_img setFrame:CGRectMake(10, 10, CGRectGetWidth([mypre bounds]), CGRectGetHeight([mypre bounds]))];
+  if (next_direction_is_up == YES) {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:1.5];
+    [animation_img setFrame:CGRectMake(10,10,20,20)];
+    [UIView commitAnimations];
+    next_direction_is_up = NO;
+  } else {    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:1.5];
+    [animation_img setFrame:CGRectMake(10,10,20,100)];
+    [UIView commitAnimations];
+    next_direction_is_up = YES;
+  }
+  
+  return;
+  
   if ([animation_img pointInside:CGPointMake(15, 60) withEvent:nil] == NO) {
          [UIView beginAnimations:nil context:NULL];
          [UIView setAnimationDuration:1.5];
@@ -50,6 +90,7 @@
   [self.view addSubview:rage_meter];
   [rage_meter startAnimating];
   [rage_meter release];
+   
 
 
   //[animation_img setBackgroundColor:hit_result.backgroundColor];
@@ -72,6 +113,18 @@
 }
 
 
+-(void)locationUpdate:(CLLocation *) location {
+  locationLabel.text = [location description];
+  
+  
+}
+
+-(void)locationError:(NSError *) error{
+  
+  locationLabel.text = [error description];
+  
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
@@ -83,6 +136,7 @@
 
 
 - (void)dealloc {
+  [locationController release];
     [super dealloc];
 }
 
